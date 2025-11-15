8bea61be81008d2d601e47d2d328ac852dfc537f
syeda-shamaim <khalidshamaim@gmail.com>
2025-11-15 14:39:21 +0500

    Add CV download button

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function DownloadButton(){
   return (
-    <a href="/cv.pdf" download="SyedaShamaimFatimaResume.pdf" className="btn cv-btn">
+    <a href="/ShamaimResume.pdf" download="SyedaShamaim.pdf" className="btn cv-btn">
       Download CV
     </a>
   )
