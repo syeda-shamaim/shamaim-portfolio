db769bd1489420b6bde4ed7e2af0a27469bda30e
syeda-shamaim <khalidshamaim@gmail.com>
2025-11-15 15:18:17 +0500

    Setup Bun + Vite React portfolio for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import { HashRouter } from 'react-router-dom'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
